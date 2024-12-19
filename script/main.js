bb6b6573a162ab192139f667d5652bdd53c91137
Kadek Adi Setiawan <adisetiawanzx@gmail.com>
2024-12-19 20:31:41 +0800

    first commit

--- script/main.js
+++ script/main.js
@@ -1,22 +1,45 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Udah siap gak?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        confirmButtonText: 'Siap',
+        cancelButtonText: 'Belum',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
             animationTimeline();
         } else {
-            animationTimeline();
+            setTimeout(() => {
+                readyornot()
+            }, 1000);
         }
     });
 });
+
+function readyornot(){
+    Swal.fire({
+        title: 'Udah siap gak?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Siap!!',
+        cancelButtonText: 'Belum',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play();
+            animationTimeline();
+        } else {
+            setTimeout(() => {
+                readyornot()
+            }, 1500);
+        }
+    });
+}
 
 
 // animation timeline
